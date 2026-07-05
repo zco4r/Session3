7c39328734e9b45feea84856508989526ab222a4
ammaralazki <alazkiyasser3@gmail.com>
2026-07-05 18:00:39 +0400

    Task 1 - Absolute Difference.

--- Session3/Program.cs
+++ Session3/Program.cs
@@ -2,8 +2,20 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+       //Task 1 - Absolute Difference 
+        
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double difference = num1 - num2;
+
+        double absoluteDifference = Math.Abs(difference);
+
+        Console.WriteLine("The absolute difference is: " + absoluteDifference);
     }
 }
