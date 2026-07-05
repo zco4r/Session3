cd90a7687fec17de60e94c10a50c325b36589f59
ammaralazki <alazkiyasser3@gmail.com>
2026-07-05 18:02:53 +0400

    Task 2 - Power & Root Explorer

--- Session3/Program.cs
+++ Session3/Program.cs
@@ -17,5 +17,19 @@ class Program
         double absoluteDifference = Math.Abs(difference);
 
         Console.WriteLine("The absolute difference is: " + absoluteDifference);
+        
+        //Task 2 - Power & Root Explorer
+
+        Console.Write("Enter a number: ");
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        double square = Math.Pow(number, 2);
+
+        double squareRoot = Math.Sqrt(number);
+
+        Console.WriteLine("The square of the number is: " + square);
+        Console.WriteLine("The square root of the number is: " + squareRoot);
+
+
     }
 }
